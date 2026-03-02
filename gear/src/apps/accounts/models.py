260debe986910e7220d47752440f2219e7279db3
Dava Hannas Putra <dava.hannas@ui.ac.id>
2026-03-02 22:10:52 +0700

    feat(accounts): implement custom user model

--- gear/src/apps/accounts/models.py
+++ gear/src/apps/accounts/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .managers import GEARUserManager
+
+from uuid import uuid4
+
 class GEARUser(AbstractUser):
 
-    phone_number = models.CharField(max_length=15)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    phone_number = models.CharField(max_length=15, unique=True)
+    username = None
+
+    USERNAME_FIELD = 'phone_number'
+
+    objects = GEARUserManager()
+
+    avatar = models.FileField(null=True)
 
     def __str__(self):
-        return self.username
-
-class Avatar(models.Model):
-
-    user = models.OneToOneField(to='GEARUser',
-                                on_delete=models.CASCADE,
-                                related_name='avatar')
-
-    file = models.FileField()
+        return self.get_full_name()
